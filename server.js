6f26669e5c2db9f9064358452bab674b5a97fb0d
NirajBurnwal <burnwalniraj2009@gmail.com>
2022-10-27 10:38:10 +0530

    make it better

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const cors = require("cors")
+
+require("./db/config")
+require("./model/userSchema")
+const router = require("./routes/router")
+
+app.use(cors())
+app.use(express.json())
+app.use(router)
+
+const port = process.env.PORT || 5001
+
+app.listen(port, ()=> {
+    console.log(`server started on port ${port}` );
+})
